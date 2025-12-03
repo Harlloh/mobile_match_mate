226d057c95c1e6c11d4355606f25bf2ab8a6a1f8
Harlloh <104266343+Harlloh@users.noreply.github.com>
2025-12-03 19:48:56 +0100

    added oriduction and local variables and also added the logo and splash screen

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -5,8 +5,8 @@ import 'react-native-url-polyfill/auto';
 
 
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? ''
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON ?? ''
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || process.env.SUPABASE_ANON
 
 
 if (!supabaseUrl || !supabaseAnonKey) {
--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'https://api.football-data.org/v4',
     headers: {
-        "X-Auth-Token": process.env.EXPO_PUBLIC_FOOTBALL_DATA_KEY,
+        "X-Auth-Token": process.env.EXPO_PUBLIC_FOOTBALL_DATA_KEY || process.env.FOOTBALL_DATA_KEY,
     },
 });
 
